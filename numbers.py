e1ff55091c7711b1dd84a195239353d7a628dcbe
Peter Ndungu <93506710+petreleven@users.noreply.github.com>
2025-02-01 11:37:37 +0300

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,10 @@
+firstnumber = input("give 1st number:")
+secondnumber = input("give 2nd number:")
+multiplication = int(firstnumber) * int(secondnumber)
+print("Answer is ", multiplication)
+
+addition = int(firstnumber)  + int(secondnumber)
+print("Addition answer is ",  addition)
+
+substraction = int(firstnumber) - int(secondumber)
+print("Substraction answer is ", substraction)
